659a19eaf3ff64d87541781b10f3bee8ecf32d1a
Zenotope <zaak.kerstetter@gmail.com>
2022-07-29 13:21:31 -0600

    minor fix

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -3,7 +3,7 @@ class DetailsController < ApplicationController
     RSpotify.authenticate("fec250ff07e1426791fdce87aa5046cd", "907dc836b8d54000a4fff30dccf59057"  )
 
     def show
-        trackId = params[:id]
+        trackId = '3S2R0EVwBSAVMd5UMgKTL0'
         track = RSpotify::Track.find(trackId).audio_features
         render json: track
     end
