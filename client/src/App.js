174d62ed36e5efeed642c57b47b3c07420456ff1
Kaegan O'Hara <kohara321@gmail.com>
2022-08-01 14:32:45 -0600

    Login screen and logic (no logout)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState, useEffect } from "react";
 import { Route, Switch } from 'react-router-dom'
-import Navbar from './Components/Navbar'
+import NavBar from './Components/NavBar'
 import Favorites from './Components/Favorites';
 import Search from './Components/Search';
 import SongDetail from './Components/SongDetail';
@@ -12,13 +12,26 @@ function App() {
   const [tracks, setTracks] = useState([])
   const [detailView, setDetailView] = useState(false)
   const [songSelection, setSongSelection] = useState({})
+  const [user, setUser] = useState(null)
+  const [isLogOn, setIsLogOn] = useState(false)
 
+  //note: can use 'helper functions'
   useEffect(() =>{
     fetch('http://localhost:3000/tracks')
     .then(res => res.json())
     .then((tracks) => setTracks(tracks))
   }, [])
 
+  //keeps user logged in after page refresh
+  useEffect(() =>{
+    fetch('/me')
+    .then(res => {
+      if (res.ok) {
+        res.json()
+        .then(user => setUser(user))
+      }
+    })
+  }, [])
 
 
   function onMoreInfoClick(e, song){
@@ -29,12 +42,26 @@ function App() {
     setDetailView(false)
   }
 
+  function loginToggle(){
+    {user ? setIsLogOn(true) : setIsLogOn(false)}
+  }
+
+  useEffect(() => {
+    const data = window.sessionStorage.getItem('MY_APP_STATE');
+    if (data !== null) setIsLogOn(JSON.parse(data));
+  }, []);
+
+  useEffect(() => {
+    window.sessionStorage.setItem('MY_APP_STATE', JSON.stringify(isLogOn))
+  }, [isLogOn]);
+
+  console.log(isLogOn)
 
   return (
     <div className="App">
-      <Navbar/>     
+      <NavBar loginToggle={loginToggle} isLogOn={isLogOn} />
       <Switch>
-        <Route exact patch="/">
+        <Route exact path="/">
           {detailView ? (
             <SongDetail goBack={goBack}
             />
@@ -42,15 +69,13 @@ function App() {
             <SearchBar />
             <Search tracks={tracks} onMoreInfoClick={onMoreInfoClick} goBack={goBack}/>
             </div>
-            
           )
         }
         </Route>
         <Route path="/login">
-          <Login />
+          <Login onLogin={setUser} loginToggle={loginToggle} />
         </Route>
       </Switch>
-
     </div>
   );
 }
